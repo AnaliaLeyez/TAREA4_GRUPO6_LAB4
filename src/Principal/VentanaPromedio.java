ef857fc59e188423782cba4b94e3a68336422690
Analia Leyez <124401921+AnaliaLeyez@users.noreply.github.com>
2025-04-27 20:19:18 -0300

    fin

--- src/Principal/VentanaPromedio.java
+++ src/Principal/VentanaPromedio.java
@@ -4,13 +4,9 @@ import java.awt.Color;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.KeyAdapter;
-import java.awt.event.KeyEvent;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import javax.swing.*;
-
-import validaciones.TipoErrores;
 import validaciones.Validar;
 
 public class VentanaPromedio extends Ventana {
